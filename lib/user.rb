0521aa0884cfcdec0f4083b53eff4b196ca1022e
Megan Folsom <mfolsom@gmail.com>
2014-02-12 11:30:48 +0000

    Add email validations

--- lib/user.rb
+++ lib/user.rb
@@ -5,10 +5,16 @@ class User
   include DataMapper::Resource
   attr_reader :password
   attr_accessor :password_confirmation
-  validates_confirmation_of :password
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
+  validates_uniqueness_of :email
 
   property :id, Serial
-  property :email, String
+  property :email, String, :required => true, :unique => true, :format => :email_address,
+  :messages => {
+    :presence => "We need your email address",
+    :is_unique => "We already have that email.",
+    :format => "Doesn't look like an email address to me..."
+  }
   property :password_digest, Text
   def password=(password)
     @password = password
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -9,7 +9,21 @@ feature "User signs up" do
   scenario "with a password that doesn't match" do
     lambda { sign_up('fredflinstone@example.com', 'pass', 'wrong') }.should change(User, :count).by(0)
     expect(current_path).to eq('/users')
-    expect(page).to have_content("sorry, your passwords don't match")
+    expect(page).to have_content("Sorry, your passwords don't match")
+  end
+  scenario "with an email that is already registered" do
+    lambda {sign_up }.should change(User, :count).by(1)
+    lambda {sign_up }.should change(User, :count).by(0)
+    expect(page).to have_content("We already have that email.")
+  end
+  scenario "with invalid email address" do
+    lambda { sign_up('fredflinstone', 'pass', 'pass') }.should change(User, :count).by(0)
+    expect(page).to have_content("Doesn't look like an email address to me...")
+  end
+  scenario "without an email address" do
+    visit 'users/new'
+    click_button "Sign up"
+    expect(page).to have_content("We need your email address")
   end
 
   def sign_up(email = "barneyrubble@example.com",
